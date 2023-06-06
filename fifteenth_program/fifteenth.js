74e5a935654f0e15bfe76ab8b25351a9293516b2
arulgoyal <arulgoyal6@gmail.com>
2023-06-06 16:24:38 +0530

    fifteenthjs_day2

---
+++ fifteenth_program/fifteenth.js
@@ -0,0 +1,33 @@
+// let main = document.getElementById('main');
+// console.log(main);
+// let nav = document.getElementById('nav');
+// console.log(nav)
+
+// let containers = document.getElementsByClassName('container');
+// console.log(containers)
+
+// let sel = document.querySelector('.container');
+// console.log('Selector returns = '+ sel)
+// let para = document.getElementById('para');
+// para.addEventListener('mouseover',function run(){
+//     alert('Mouse Inside');
+// });
+// para.addEventListener('mouseout', function run(){
+//     alert('Mouse Outside')
+// })
+// function togglehide(){
+//     let btn = document.getElementById('btn')
+//     let para = document.getElementById('para')
+//     if(para.style.display != 'none'){
+//     para.style.display = 'none';
+//     }
+//     else{
+//         para.style.display = 'block';
+//     }
+// }
+function greet(){
+    console.log('Hello World');
+}
+setTimeout(greet, 5000);
+
+  
